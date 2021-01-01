40ffccbc83d09c5907420984c50adf7710f5fffd
Stevö <stevehuwjohn@gmail.com>
2021-01-01 06:03:10 +0000

    WIP

---
+++ src/Sudoku.Tests/Engine/LogicTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Sudoku.Tests.Engine
+{
+    public class LogicTests
+    {
+        [Fact]
+        public void IsLegalMove_will_not_allow_duplicate_X_values()
+        {
+        }
+
+        [Fact]
+        public void IsLegalMove_will_not_allow_duplicate_Y_values()
+        {
+        }
+    }
+}
