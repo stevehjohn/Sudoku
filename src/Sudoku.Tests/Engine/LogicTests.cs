d1ff77d4a1c637cad65e07ad422af5eb7509a69b
Stevö <stevehuwjohn@gmail.com>
2021-01-03 20:22:07 +0000

    WIP

--- src/Sudoku.Tests/Engine/LogicTests.cs
+++ src/Sudoku.Tests/Engine/LogicTests.cs
@@ -30,8 +30,24 @@ namespace Sudoku.Tests.Engine
             Assert.False(Logic.IsLegalMove(board, 0, 0, 1));
         }
 
-        [Fact]
-        public void IsLegalMove_will_not_allow_duplicate_sector_values()
+        [Theory]
+        [InlineData(0, 0, false)]
+        [InlineData(1, 0, false)]
+        [InlineData(2, 0, false)]
+        [InlineData(3, 0, true)]
+        [InlineData(0, 1, false)]
+        [InlineData(1, 1, false)]
+        [InlineData(2, 1, false)]
+        [InlineData(3, 1, false)]
+        [InlineData(0, 2, false)]
+        [InlineData(1, 2, false)]
+        [InlineData(2, 2, false)]
+        [InlineData(3, 2, true)]
+        [InlineData(0, 3, true)]
+        [InlineData(1, 3, false)]
+        [InlineData(2, 3, true)]
+        [InlineData(3, 3, true)]
+        public void IsLegalMove_will_not_allow_duplicate_sector_values(int x, int y, bool legal)
         {
             var board = new Board();
 
@@ -39,7 +55,7 @@ namespace Sudoku.Tests.Engine
 
             board.Poke(1, 1, 1);
 
-            Assert.False(Logic.IsLegalMove(board, 0, 0, 1));
+            Assert.Equal(Logic.IsLegalMove(board, x, y, 1), legal);
         }
     }
 }
