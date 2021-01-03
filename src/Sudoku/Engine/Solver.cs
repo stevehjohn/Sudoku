b1ad5c32ae3927e6923863d0954ad81ad1152b52
Stevö <stevehuwjohn@gmail.com>
2021-01-03 20:59:02 +0000

    WIP

--- src/Sudoku/Engine/Solver.cs
+++ src/Sudoku/Engine/Solver.cs
@@ -6,6 +6,31 @@ namespace Sudoku.Engine
     {
         public void Solve(Board input)
         {
+            var startIdx = 1;
+
+            var initialState = input.SaveState();
+
+            //while (true)
+            {
+                var board = new Board();
+
+                board.LoadState(initialState);
+
+                for (var x = 0; x < Constants.BoardSize; x++)
+                {
+                    for (var y = 0; y < Constants.BoardSize; y++)
+                    {
+                        if (board.Peek(x, y) != 0)
+                        {
+                            continue;
+                        }
+
+                        for (var i = 1; i < 10; i++)
+                        {
+                        }
+                    }
+                }
+            }
         }
     }
 }
