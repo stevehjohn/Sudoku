5f68226d6921861e080148b46897bda1abc1f450
Stevö <stevehuwjohn@gmail.com>
2021-01-03 20:29:25 +0000

    WIP

--- src/Sudoku.Tests/Infrastructure/BoardTests.cs
+++ src/Sudoku.Tests/Infrastructure/BoardTests.cs
@@ -1,7 +1,19 @@
-﻿namespace Sudoku.Tests.Infrastructure
+﻿using Sudoku.Exceptions;
+using Sudoku.Infrastructure;
+using Xunit;
+
+namespace Sudoku.Tests.Infrastructure
 {
     public class BoardTests
     {
-        
+        [Fact]
+        public void Poke_throws_exception_on_illegal_move()
+        {
+            var board = new Board();
+
+            board.Poke(0, 0, 1);
+
+            Assert.Throws<BoardException>(() => board.Poke(0, 1, 1));
+        }
     }
 }
