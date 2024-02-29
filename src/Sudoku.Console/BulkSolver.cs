9a95d21555f4db25a603a3f4620ee2c1dcebb0d1
Stevo John <stevehuwjohn@gmail.com>
2024-02-29 11:44:18 +0000

    WIP

--- src/Sudoku.Console/BulkSolver.cs
+++ src/Sudoku.Console/BulkSolver.cs
@@ -99,7 +99,7 @@ public class BulkSolver
 
         System.Console.Write($"\n All puzzles solved in: {_stopwatch.Elapsed.Minutes:N0}:{_stopwatch.Elapsed.Seconds:D2}. ");
         
-        System.Console.WriteLine($"{solved / _stopwatch.Elapsed.TotalSeconds} puzzles/sec.");
+        System.Console.WriteLine($"{solved / _stopwatch.Elapsed.TotalSeconds:N0} puzzles/sec.");
         
         System.Console.CursorVisible = true;
     }
