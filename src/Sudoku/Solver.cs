c54a7236b6b7eff0f4102ffc886052bd37a43fb3
Stevo John <stevehuwjohn@gmail.com>
2024-03-01 15:22:57 +0000

    Faster

--- src/Sudoku/Solver.cs
+++ src/Sudoku/Solver.cs
@@ -17,6 +17,99 @@ public class Solver
     private readonly PriorityQueue<(int[] Sudoku, bool Solved, List<Move> History), int> _stepSolutions = new();
 
     private readonly Stack<(int[] Puzzle, List<Move> History)> _stack = [];
+
+    private static readonly (int Row, int Column, int Box)[] Lookup = 
+    [
+        (0, 0, 0),
+        (0, 1, 0),
+        (0, 2, 0),
+        (0, 3, 1),
+        (0, 4, 1),
+        (0, 5, 1),
+        (0, 6, 2),
+        (0, 7, 2),
+        (0, 8, 2),
+
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 2, 0),
+        (1, 3, 1),
+        (1, 4, 1),
+        (1, 5, 1),
+        (1, 6, 2),
+        (1, 7, 2),
+        (1, 8, 2),
+
+        (2, 0, 0),
+        (2, 1, 0),
+        (2, 2, 0),
+        (2, 3, 1),
+        (2, 4, 1),
+        (2, 5, 1),
+        (2, 6, 2),
+        (2, 7, 2),
+        (2, 8, 2),
+
+        (3, 0, 3),
+        (3, 1, 3),
+        (3, 2, 3),
+        (3, 3, 4),
+        (3, 4, 4),
+        (3, 5, 4),
+        (3, 6, 5),
+        (3, 7, 5),
+        (3, 8, 5),
+
+        (4, 0, 3),
+        (4, 1, 3),
+        (4, 2, 3),
+        (4, 3, 4),
+        (4, 4, 4),
+        (4, 5, 4),
+        (4, 6, 5),
+        (4, 7, 5),
+        (4, 8, 5),
+
+        (5, 0, 3),
+        (5, 1, 3),
+        (5, 2, 3),
+        (5, 3, 4),
+        (5, 4, 4),
+        (5, 5, 4),
+        (5, 6, 5),
+        (5, 7, 5),
+        (5, 8, 5),
+
+        (6, 0, 6),
+        (6, 1, 6),
+        (6, 2, 6),
+        (6, 3, 7),
+        (6, 4, 7),
+        (6, 5, 7),
+        (6, 6, 8),
+        (6, 7, 8),
+        (6, 8, 8),
+
+        (7, 0, 6),
+        (7, 1, 6),
+        (7, 2, 6),
+        (7, 3, 7),
+        (7, 4, 7),
+        (7, 5, 7),
+        (7, 6, 8),
+        (7, 7, 8),
+        (7, 8, 8),
+
+        (8, 0, 6),
+        (8, 1, 6),
+        (8, 2, 6),
+        (8, 3, 7),
+        (8, 4, 7),
+        (8, 5, 7),
+        (8, 6, 8),
+        (8, 7, 8),
+        (8, 8, 8)
+    ];
     
     public (int[] Solution, int Steps, double Microseconds, List<Move> History) Solve(int[] sudoku, bool record = false)
     {
@@ -66,40 +159,26 @@ public class Solver
     
     private void SolveStep(int[] sudoku, List<Move> history)
     {
-        for (var y = 0; y < 9; y++)
+        for (var i = 0; i < 9; i++)
         {
-            _rowCandidates[y] = 0b11_1111_1111;
+            _rowCandidates[i] = 0b11_1111_1111;
 
-            _columnCandidates[y] = 0b11_1111_1111;
-
-            var y9 = y * 9;
-
-            for (var x = 0; x < 9; x++)
-            {
-                _rowCandidates[y] &= ~(1 << sudoku[x + y9]);
-
-                _columnCandidates[y] &= ~(1 << sudoku[y + x * 9]);
-            }
+            _columnCandidates[i] = 0b11_1111_1111;
+            
+            _boxCandidates[i] = 0b11_1111_1111;
         }
 
-        for (var y = 0; y < 9; y += 3)
+        for (var i = 0; i < 81; i++)
         {
-            for (var x = 0; x < 3; x++)
-            {
-                _boxCandidates[y + x] = 0b11_1111_1111;
+            var lookup = Lookup[i];
 
-                var x3 = x * 3;
-
-                for (var y1 = 0; y1 < 3; y1++)
-                {
-                    var yy1 = y + y1;
-
-                    for (var x1 = 0; x1 < 3; x1++)
-                    {
-                        _boxCandidates[y + x] &= ~(1 << sudoku[x3 + x1 + yy1 * 9]);
-                    }
-                }
-            }
+            var value = ~(1 << sudoku[i]);
+            
+            _rowCandidates[lookup.Row] &= value;
+            
+            _columnCandidates[lookup.Column] &= value;
+            
+            _boxCandidates[lookup.Box] &= value;
         }
 
         var position = (X: -1, Y: -1);
