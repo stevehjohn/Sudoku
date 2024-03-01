cc2bacdc6b9b855d7039187ef293f2d095e634b8
Stevo John <stevehuwjohn@gmail.com>
2024-03-01 17:21:48 +0000

    Test

--- src/Sudoku/Solver.cs
+++ src/Sudoku/Solver.cs
@@ -18,101 +18,7 @@ public class Solver
 
     private readonly Stack<(int[] Puzzle, List<(int Position, int Value)> History)> _stack = [];
 
-    private static readonly (int Row, int Column, int Box)[] LookupTable = 
-    [
-        (0, 0, 0),
-        (0, 1, 0),
-        (0, 2, 0),
-        (0, 3, 1),
-        (0, 4, 1),
-        (0, 5, 1),
-        (0, 6, 2),
-        (0, 7, 2),
-        (0, 8, 2),
-
-        (1, 0, 0),
-        (1, 1, 0),
-        (1, 2, 0),
-        (1, 3, 1),
-        (1, 4, 1),
-        (1, 5, 1),
-        (1, 6, 2),
-        (1, 7, 2),
-        (1, 8, 2),
-
-        (2, 0, 0),
-        (2, 1, 0),
-        (2, 2, 0),
-        (2, 3, 1),
-        (2, 4, 1),
-        (2, 5, 1),
-        (2, 6, 2),
-        (2, 7, 2),
-        (2, 8, 2),
-
-        (3, 0, 3),
-        (3, 1, 3),
-        (3, 2, 3),
-        (3, 3, 4),
-        (3, 4, 4),
-        (3, 5, 4),
-        (3, 6, 5),
-        (3, 7, 5),
-        (3, 8, 5),
-
-        (4, 0, 3),
-        (4, 1, 3),
-        (4, 2, 3),
-        (4, 3, 4),
-        (4, 4, 4),
-        (4, 5, 4),
-        (4, 6, 5),
-        (4, 7, 5),
-        (4, 8, 5),
-
-        (5, 0, 3),
-        (5, 1, 3),
-        (5, 2, 3),
-        (5, 3, 4),
-        (5, 4, 4),
-        (5, 5, 4),
-        (5, 6, 5),
-        (5, 7, 5),
-        (5, 8, 5),
-
-        (6, 0, 6),
-        (6, 1, 6),
-        (6, 2, 6),
-        (6, 3, 7),
-        (6, 4, 7),
-        (6, 5, 7),
-        (6, 6, 8),
-        (6, 7, 8),
-        (6, 8, 8),
-
-        (7, 0, 6),
-        (7, 1, 6),
-        (7, 2, 6),
-        (7, 3, 7),
-        (7, 4, 7),
-        (7, 5, 7),
-        (7, 6, 8),
-        (7, 7, 8),
-        (7, 8, 8),
-
-        (8, 0, 6),
-        (8, 1, 6),
-        (8, 2, 6),
-        (8, 3, 7),
-        (8, 4, 7),
-        (8, 5, 7),
-        (8, 6, 8),
-        (8, 7, 8),
-        (8, 8, 8)
-    ];
-
-    public unsafe (int[] Solution, int Steps, double Microseconds, List<(int Position, int Value)> History) Solve(
-        int[] sudoku, bool record = false)
+    public (int[] Solution, int Steps, double Microseconds, List<(int Position, int Value)> History) Solve(int[] sudoku, bool record = false)
     {
         _stepSolutions.Clear();
 
@@ -124,44 +30,50 @@ public class Solver
 
         var stopwatch = Stopwatch.StartNew();
 
-        fixed ((int, int, int)* lookup = &LookupTable[0])
+        while (_stack.TryPop(out var item))
         {
-            while (_stack.TryPop(out var item))
+            steps++;
+
+            SolveStep(item.Puzzle, item.History);
+
+            if (steps > 1)
             {
-                steps++;
-
-                SolveStep(item.Puzzle, item.History, lookup);
-
-                if (steps > 1)
-                {
-                    _pool.Return(item.Puzzle);
-                }
-
-                while (_stepSolutions.TryDequeue(out var solution, out _))
-                {
-                    if (solution.Solved)
-                    {
-                        stopwatch.Stop();
-
-                        while (_stack.TryPop(out item))
-                        {
-                            _pool.Return(item.Puzzle);
-                        }
-
-                        return (solution.Sudoku, steps, stopwatch.Elapsed.TotalMicroseconds, solution.History);
-                    }
-
-                    _stack.Push((solution.Sudoku, solution.History));
-                }
+                _pool.Return(item.Puzzle);
             }
 
-            stopwatch.Stop();
+            while (_stepSolutions.TryDequeue(out var solution, out _))
+            {
+                if (solution.Solved)
+                {
+                    stopwatch.Stop();
 
-            return (null, steps, stopwatch.Elapsed.TotalMicroseconds, null);
+                    while (_stack.TryPop(out item))
+                    {
+                        _pool.Return(item.Puzzle);
+                    }
+
+                    return (solution.Sudoku, steps, stopwatch.Elapsed.TotalMicroseconds, solution.History);
+                }
+
+                _stack.Push((solution.Sudoku, solution.History));
+            }
         }
+
+        stopwatch.Stop();
+
+        return (null, steps, stopwatch.Elapsed.TotalMicroseconds, null);
     }
 
-    private unsafe void SolveStep(int[] sudoku, List<(int Position, int Value)> history, (int Row, int Column, int Box)* lookup)
+    private (int Row, int Column, int Box) Lookup(int index)
+    {
+        var row = index / 9;
+
+        var column = index % 9;
+
+        return (row, column, row / 3 * 3 + column / 3);
+    }
+
+    private void SolveStep(int[] sudoku, List<(int Position, int Value)> history)
     {
         for (var i = 0; i < 9; i++)
         {
@@ -174,7 +86,7 @@ public class Solver
 
         for (var i = 0; i < 81; i++)
         {
-            var pointer = lookup[i];
+            var pointer = Lookup(i);
 
             var value = ~(1 << sudoku[i]);
             
@@ -198,7 +110,7 @@ public class Solver
                 continue;
             }
 
-            var pointer = lookup[i];
+            var pointer = Lookup(i);
 
             var row = _rowCandidates[pointer.Row];
 
