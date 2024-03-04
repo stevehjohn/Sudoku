86c34fe7c530c97151075ac8cbd021b8ce46186f
Stevo <stevehuwjohn@gmail.com>
2024-03-04 22:25:30 +0000

    WIP

--- src/Sudoku/Solver.cs
+++ src/Sudoku/Solver.cs
@@ -48,6 +48,8 @@ public class Solver
     
     private bool SolveStep(Span<int> puzzle, int score, ref int steps, List<Move> history)
     {
+        FindHiddenSingles();
+        
         var move = FindLowestMove(puzzle);
 
         return CreateNextSteps(puzzle, move, score, ref steps, history);
@@ -107,6 +109,99 @@ public class Solver
         }
     }
 
+    private void FindHiddenSingles()
+    {
+        for (var y = 0; y < 9; y++)
+        {
+            var oneMaskRow = 0;
+        
+            var twoMaskRow = 0;
+        
+            var oneMaskColumn = 0;
+        
+            var twoMaskColumn = 0;
+        
+            for (var x = 0; x < 9; x++)
+            {
+                twoMaskRow |= oneMaskRow & _cellCandidates[(y << 3) + y + x];
+        
+                oneMaskRow |= _cellCandidates[(y << 3) + y + x];
+
+                twoMaskColumn |= oneMaskColumn & _cellCandidates[(x << 3) + x + y];
+        
+                oneMaskColumn |= _cellCandidates[(x << 3) + x + y];
+            }
+        
+            var onceRow = oneMaskRow & ~twoMaskRow;
+        
+            var onceColumn = oneMaskColumn & ~twoMaskColumn;
+        
+            if (BitOperations.PopCount((uint) onceRow) == 1)
+            {
+                for (var x = 0; x < 9; x++)
+                {
+                    if ((_cellCandidates[(y << 3) + y + x] & onceRow) > 0)
+                    {
+                        _cellCandidates[(y << 3) + y + x] = onceRow;
+
+                        return;
+                    }
+                }
+            }
+        
+            if (BitOperations.PopCount((uint) onceColumn) == 1)
+            {
+                for (var x = 0; x < 9; x++)
+                {
+                    if ((_cellCandidates[(x << 3) + x + y] & onceColumn) > 0)
+                    {
+                        _cellCandidates[(x << 3) + x + y] = onceColumn;
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        for (var yO = 0; yO < 81; yO += 27)
+        {
+            for (var xO = 0; xO < 9; xO += 3)
+            {
+                var oneMask = 0;
+
+                var twoMask = 0;
+
+                var start = yO + xO;
+
+                for (var y = 0; y < 3; y++)
+                {
+                    for (var x = 0; x < 3; x++)
+                    {
+                        twoMask |= oneMask & _cellCandidates[start + (y << 3) + y + x];
+
+                        oneMask |= _cellCandidates[start + (y << 3) + y + x];
+                    }
+                }
+
+                var once = oneMask & ~twoMask;
+
+                if (BitOperations.PopCount((uint) once) == 1)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        for (var x = 0; x < 3; x++)
+                        {
+                            if ((_cellCandidates[start + (y << 3) + y + x] & once) > 0)
+                            {
+                                _cellCandidates[start + (y << 3) + y + x] = once;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     private ((int X, int Y) Position, int Values, int ValueCount) FindLowestMove(Span<int> puzzle)
     {
         var position = (X: -1, Y: -1);
