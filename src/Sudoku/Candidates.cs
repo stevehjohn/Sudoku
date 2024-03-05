381e75b811fabae058e8481f055171c90d6de208
Stevo <stevehuwjohn@gmail.com>
2024-03-05 17:00:59 +0000

    WIP

--- src/Sudoku/Candidates.cs
+++ src/Sudoku/Candidates.cs
@@ -38,20 +38,5 @@ public struct Candidates
 
             return (int) (_low >> ((index - 5) * 9)) & 0b1_1111_1111;
         }
-        set
-        {
-            if (index < 5)
-            {
-                _high &= ~(0b1_1111_1111ul << (index * 9));
-        
-                _high |= (ulong) value << (index * 9);
-            }
-            else
-            {
-                _low &= ~(0b1_1111_1111ul << ((index - 5) * 9));
-        
-                _low |= (ulong) value << ((index - 5) * 9);
-            }
-        }
     }
 }
