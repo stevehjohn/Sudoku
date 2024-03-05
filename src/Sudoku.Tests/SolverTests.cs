a12783e4d9472daef03da9d582dadd837529b79f
Stevo <stevehuwjohn@gmail.com>
2024-03-05 16:38:31 +0000

    Adding tests.

---
+++ src/Sudoku.Tests/SolverTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sudoku.Tests;
+
+public class SolverTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public SolverTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void VerifySolverProducesCorrectResults()
+    {
+        var puzzles = File.ReadAllLines("Test Data/Puzzles With Answers.sudoku");
+
+        var solver = new Solver.Solver();
+
+        var stopwatch = Stopwatch.StartNew();
+        
+        foreach (var puzzle in puzzles)
+        {
+            var parts = puzzle.Split(',');
+
+            var input = new int[81];
+            
+            for (var i = 0; i < 81; i++)
+            {
+                input[i] = parts[0][i] - '0';
+            }
+
+            var result = solver.Solve(input);
+            
+            for (var i = 0; i < 81; i++)
+            {
+                if (result.Solution[i] != parts[1][i] - '0')
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+        
+        stopwatch.Stop();
+        
+        _testOutputHelper.WriteLine($"{puzzles.Length} puzzles verified in {stopwatch.Elapsed.TotalMilliseconds}ms.");
+    }
+}
