594eee4ff6c1976ab917a49570479921c29f0014
Stevo <stevehuwjohn@gmail.com>
2024-03-09 16:18:28 +0000

    WIP

--- src/Sudoku.Console/ConsoleSolveVisualiser.cs
+++ src/Sudoku.Console/ConsoleSolveVisualiser.cs
@@ -6,14 +6,16 @@ public class ConsoleSolveVisualiser
 {
     private readonly int[] _puzzle;
 
-    private List<Move> _history;
+    private readonly List<Move> _history;
 
-    private List<int>[] _initialCandidates;
+    private readonly List<int>[] _initialCandidates;
 
     private int _left;
 
     private int _top;
 
+    private readonly ConsoleColor _consoleColor;
+    
     public ConsoleSolveVisualiser(int[] puzzle, List<Move> history, List<int>[] initialCandidates)
     {
         _puzzle = puzzle;
@@ -21,6 +23,8 @@ public class ConsoleSolveVisualiser
         _history = history;
 
         _initialCandidates = initialCandidates;
+
+        _consoleColor = Out.ForegroundColor;
     }
 
     public void Visualise(int left, int top)
@@ -37,9 +41,68 @@ public class ConsoleSolveVisualiser
 
         DrawPuzzle();
 
+        DrawInitialCandidates();
+
+        ShowText("Press S to start");
+
+        while (char.ToLower(Out.ReadKey(true).KeyChar) != 's')
+        {
+        }
+        
+        ShowText("");
+        
         Out.CursorTop = 39;
         
         Out.CursorVisible = true;
+    }
+
+    private void ShowText(string text)
+    {
+        Out.CursorTop = 1;
+
+        Out.CursorLeft = _left;
+
+        var whitespace = new string(' ', 28 - text.Length / 2);
+        
+        Out.Write($"{whitespace}{text}{whitespace}");
+    }
+
+    private void DrawInitialCandidates()
+    {
+        Out.ForegroundColor = ConsoleColor.Blue;
+        
+        for (var y = 0; y < 9; y++)
+        {
+            for (var x = 0; x < 9; x++)
+            {
+                if (_initialCandidates[x + y * 9] != null)
+                {
+                    foreach (var candidate in _initialCandidates[x + y * 9])
+                    {
+                        SetCursorPosition(x, y, candidate);
+                        
+                        Out.Write(candidate);
+                    }
+                }
+            }
+        }
+
+        Out.ForegroundColor = _consoleColor;
+    }
+
+    private void SetCursorPosition(int x, int y, int value)
+    {
+        var top = _top + 2 + y * 4;
+
+        var left = _left + 1 + x * 6;
+
+        top += (value - 1) / 3;
+
+        left += (value - 1) % 3 * 2;
+        
+        Out.CursorTop = top;
+
+        Out.CursorLeft = left;
     }
 
     private void DrawPuzzle()
