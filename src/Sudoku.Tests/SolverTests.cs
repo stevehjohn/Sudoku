08546acf8b4ae2415e64be575e267a45daff83ae
Stevo <stevehuwjohn@gmail.com>
2024-03-12 13:23:27 +0000

    WIP

--- src/Sudoku.Tests/SolverTests.cs
+++ src/Sudoku.Tests/SolverTests.cs
@@ -14,6 +14,49 @@ public class SolverTests
     }
 
     [Fact]
+    public void VerifySolverDetectsInvalidPuzzles()
+    {
+        var puzzles = File.ReadAllLines("Test Data/Invalid Puzzles.txt");
+
+        var solver = new Solver();
+
+        var stopwatch = Stopwatch.StartNew();
+        
+        foreach (var puzzle in puzzles)
+        {
+            var parts = puzzle.Split(',');
+
+            var input = new int[81];
+            
+            for (var i = 0; i < 81; i++)
+            {
+                input[i] = parts[0][i] - '0';
+            }
+
+            try
+            {
+                var result = solver.Solve(input);
+            }
+            catch
+            {
+                //
+            }
+
+            for (var i = 0; i < 81; i++)
+            {
+                if (result.Solution[i] != parts[1][i] - '0')
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+        
+        stopwatch.Stop();
+        
+        _testOutputHelper.WriteLine($"{puzzles.Length:N0} puzzles verified in {stopwatch.Elapsed.TotalMilliseconds:N3}ms.");
+    }
+
+    [Fact]
     public void VerifySolverProducesCorrectResults()
     {
         var puzzles = File.ReadAllLines("Test Data/Puzzles With Answers.txt");
