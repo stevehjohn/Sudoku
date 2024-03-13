afcadf51a4a1688c79c0c8b77cc7227fba74aff0
Stevo <stevehuwjohn@gmail.com>
2024-03-13 12:26:48 +0000

    WIP

---
+++ src/Sudoku/Extensions/SpanIntExtensions.cs
@@ -0,0 +1,76 @@
+namespace Sudoku.Extensions;
+
+public static class SpanIntExtensions
+{
+    public static bool IsValidSudoku(this Span<int> puzzle)
+    {
+        var uniqueRow = new HashSet<int>();
+
+        var uniqueColumn = new HashSet<int>();
+
+        for (var y = 0; y < 9; y++)
+        {
+            uniqueRow.Clear();
+
+            uniqueColumn.Clear();
+
+            var countRow = 0;
+
+            var countColumn = 0;
+
+            for (var x = 0; x < 9; x++)
+            {
+                if (puzzle[x + y * 9] != 0)
+                {
+                    uniqueRow.Add(puzzle[x + y * 9]);
+
+                    countRow++;
+                }
+
+                if (puzzle[y + x * 9] != 0)
+                {
+                    uniqueColumn.Add(puzzle[y + x * 9]);
+
+                    countColumn++;
+                }
+            }
+
+            if (uniqueRow.Count < countRow || uniqueColumn.Count < countColumn)
+            {
+                return false;
+            }
+        }
+
+        var uniqueBox = new HashSet<int>();
+
+        for (var yO = 0; yO < 9; yO += 3)
+        {
+            for (var xO = 0; xO < 9; xO += 3)
+            {
+                uniqueBox.Clear();
+
+                var countBox = 0;
+
+                for (var x = 0; x < 3; x++)
+                {
+                    for (var y = 0; y < 3; y++)
+                    {
+                        if (puzzle[(yO + y) * 9 + xO + x] != 0)
+                        {
+                            uniqueBox.Add(puzzle[(yO + y) * 9 + xO + x]);
+
+                            countBox++;
+                        }
+                    }
+                }
+
+                if (uniqueBox.Count < countBox)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
--- src/Sudoku/Generator.cs
+++ src/Sudoku/Generator.cs
@@ -1,3 +1,5 @@
+using Sudoku.Extensions;
+
 namespace Sudoku;
 
 public class Generator
@@ -77,7 +79,7 @@ public class Generator
 
             puzzle[cell] = candidate;
 
-            if (IsValid(puzzle))
+            if (puzzle.IsValidSudoku())
             {
                 if (cell == 80)
                 {
@@ -93,79 +95,6 @@ public class Generator
         _candidates[cell] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         return CreateSolvedPuzzle(puzzle, cell - 1);
-    }
-
-    private static bool IsValid(Span<int> puzzle)
-    {
-        var uniqueRow = new HashSet<int>();
-
-        var uniqueColumn = new HashSet<int>();
-
-        for (var y = 0; y < 9; y++)
-        {
-            uniqueRow.Clear();
-            
-            uniqueColumn.Clear();
-
-            var countRow = 0;
-
-            var countColumn = 0;
-
-            for (var x = 0; x < 9; x++)
-            {
-                if (puzzle[x + y * 9] != 0)
-                {
-                    uniqueRow.Add(puzzle[x + y * 9]);
-
-                    countRow++;
-                }
-
-                if (puzzle[y + x * 9] != 0)
-                {
-                    uniqueColumn.Add(puzzle[y + x * 9]);
-
-                    countColumn++;
-                }
-            }
-
-            if (uniqueRow.Count < countRow || uniqueColumn.Count < countColumn)
-            {
-                return false;
-            }
-        }
-
-        var uniqueBox = new HashSet<int>();
-
-        for (var yO = 0; yO < 9; yO += 3)
-        {
-            for (var xO = 0; xO < 9; xO += 3)
-            {
-                uniqueBox.Clear();
-                
-                var countBox = 0;
-
-                for (var x = 0; x < 3; x++)
-                {
-                    for (var y = 0; y < 3; y++)
-                    {
-                        if (puzzle[(yO + y) * 9 + xO + x] != 0)
-                        {
-                            uniqueBox.Add(puzzle[(yO + y) * 9 + xO + x]);
-
-                            countBox++;
-                        }
-                    }
-                }
-
-                if (uniqueBox.Count < countBox)
-                {
-                    return false;
-                }
-            }
-        }
-
-        
-        return true;
     }
 
     private void InitialiseCandidates()
