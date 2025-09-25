a832b3858bd686f2c441253de13dc2b4dfe2ccdd
Stevo John <stevehuwjohn@gmail.com>
2025-09-25 18:02:08 +0100

    Tweaking

--- src/Sudoku.Console/ConsoleApplication.cs
+++ src/Sudoku.Console/ConsoleApplication.cs
@@ -290,7 +290,7 @@ public class ConsoleApplication
             File.Delete(filename);
         }
 
-        Out($"\n Generating {clues} clue puzzles...\n");
+        Out($"\n {DateTime.Now:HH:mm:ss}: Generating {clues} clue puzzle{(puzzleCount > 1 ? "s" : string.Empty)}...\n");
 
         var recent = new List<int[]>();
 
