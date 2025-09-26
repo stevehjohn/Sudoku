9e1a981fae7585607957eb4f377b1311f9a09252
Stevo John <stevehuwjohn@gmail.com>
2025-09-26 15:39:28 +0100

    WIP

--- src/Sudoku/Generator.cs
+++ src/Sudoku/Generator.cs
@@ -52,13 +52,7 @@ public class Generator
 
                 i--;
             }
-            
-            Console.Write($"{i}   ");
-
-            Console.CursorLeft = 0;
         }
-        
-        Console.WriteLine();
     }
 
     private bool CreateSolvedPuzzle(Span<int> puzzle, int cell = 0)
