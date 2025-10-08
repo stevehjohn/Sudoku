c32e4112c8715d8d57cf801c2e98df3d34d637de
Stevo John <stevehuwjohn@gmail.com>
2025-10-08 16:38:14 +0100

    WIP

--- src/Sudoku/NakedPairMetadata.cs
+++ src/Sudoku/NakedPairMetadata.cs
@@ -2,10 +2,10 @@ namespace Sudoku;
 
 public struct NakedPairMetadata
 {
-    public List<int> Pair { get; set; }
+    public List<int> Pair { get; }
 
-    public List<int> Affected { get; set; }
-
+    public List<int> Affected { get; }
+    
     public NakedPairMetadata()
     {
         Pair = [];
