b08dfe9f31bf8877034f54858319911c40c1e548
Stevo John <stevehuwjohn@gmail.com>
2025-11-05 15:12:58 +0000

    Tweak

--- src/Sudoku/Generator.cs
+++ src/Sudoku/Generator.cs
@@ -105,8 +105,6 @@ public class Generator
 
         var stopWatch = Stopwatch.StartNew();
         
-        Array.Fill(_failed, false);
-        
         return RemoveCell(puzzle, targetClues, cellsToRemove, stopWatch, budgetSeconds * Stopwatch.Frequency, 0, cancellationToken);
     }
 
@@ -260,6 +258,8 @@ public class Generator
             _candidates[i] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
             _candidateCounts[i] = 9;
+
+            _failed[i] = false;
         }
     }
 }
