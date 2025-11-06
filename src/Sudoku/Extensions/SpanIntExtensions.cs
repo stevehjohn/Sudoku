6b0479a3e9c149c70c18f433c922875c7b82c096
Stevo John <stevehuwjohn@gmail.com>
2025-11-06 12:33:38 +0000

    Code clean

--- src/Sudoku/Extensions/SpanIntExtensions.cs
+++ src/Sudoku/Extensions/SpanIntExtensions.cs
@@ -5,11 +5,11 @@ public static class SpanIntExtensions
     public static bool IsValidSudoku(this Span<int> puzzle)
     {
         Span<ushort> rowMask = stackalloc ushort[9];
-        
+
         Span<ushort> columnMask = stackalloc ushort[9];
-        
+
         Span<ushort> boxMask = stackalloc ushort[9];
-    
+
         for (var i = 0; i < 81; i++)
         {
             var value = puzzle[i];
@@ -18,27 +18,27 @@ public static class SpanIntExtensions
             {
                 continue;
             }
-        
-            var bit = (ushort)(1 << value);
-            
+
+            var bit = (ushort) (1 << value);
+
             var row = i / 9;
-            
+
             var column = i % 9;
-            
+
             var box = row / 3 * 3 + column / 3;
 
             if (((rowMask[row] | columnMask[column] | boxMask[box]) & bit) != 0)
             {
                 return false;
             }
-        
+
             rowMask[row] |= bit;
-            
+
             columnMask[column] |= bit;
-            
+
             boxMask[box] |= bit;
         }
-    
+
         return true;
     }
 }
