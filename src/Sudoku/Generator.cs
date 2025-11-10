1e8b928889de6a20e3a2ea92e1ec6f0c4d8a9634
Stevo John <stevehuwjohn@gmail.com>
2025-11-10 18:01:17 +0000

    Tweak

--- src/Sudoku/Generator.cs
+++ src/Sudoku/Generator.cs
@@ -281,7 +281,7 @@ public class Generator
                 return false;
             }
 
-            var unique = _solver.HasUniqueSolution(puzzle, _originalPuzzle);
+            var unique = i < 9 || _solver.HasUniqueSolution(puzzle, _originalPuzzle);
 
             if (unique && RemoveCell(puzzle, targetClues, cellsToRemove - 1, stopwatch, budgetTicks, i + 1, cancellationToken))
             {
