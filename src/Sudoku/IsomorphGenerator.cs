4203ece61b19dd05d1472b35170eb98f0288cf56
Stevo John <stevehuwjohn@gmail.com>
2025-11-12 17:56:38 +0000

    WIP

--- src/Sudoku/IsomorphGenerator.cs
+++ src/Sudoku/IsomorphGenerator.cs
@@ -1,3 +1,5 @@
+using Sudoku.Extensions;
+
 namespace Sudoku;
 
 public static class IsomorphGenerator
@@ -5,19 +7,17 @@ public static class IsomorphGenerator
     public static List<int[]> CreateIsomorphs(int count, int[] puzzle)
     {
         var isomorphs = new List<int[]>();
-        
+
         while (count > 0)
         {
             var isomorph = new int[81];
-            
+
             isomorphs.Add(isomorph);
-            
+
             Array.Copy(puzzle, isomorph, 81);
 
-            // TODO: Morphin'
-            
             RelabelDigits(puzzle);
-            
+
             count--;
         }
 
@@ -26,5 +26,20 @@ public static class IsomorphGenerator
 
     private static void RelabelDigits(Span<int> puzzle)
     {
+        Span<int> digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        digits.Shuffle();
+
+        for (var i = 0; i < 81; i++)
+        {
+            var cell = puzzle[i];
+            
+            if (cell == 0)
+            {
+                continue;
+            }
+
+            puzzle[i] = digits[cell - 1];
+        }
     }
 }
