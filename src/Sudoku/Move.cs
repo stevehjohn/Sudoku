ce30bc25e696161ac52739cdd817b53cd43ac8ec
Stevo John <stevehuwjohn@gmail.com>
2025-11-13 18:05:21 +0000

    Clean

--- src/Sudoku/Move.cs
+++ src/Sudoku/Move.cs
@@ -12,8 +12,6 @@ public struct Move
 
     public int[] Candidates { get; set; } = [];
 
-    private object _metadata;
-
     public Move(int x, int y, int value, MoveType moveType)
     {
         X = x;
@@ -23,15 +21,5 @@ public struct Move
         Value = value;
 
         Type = moveType;
-    }
-    
-    public void AddMetadata<T>(T metadata)
-    {
-        _metadata = metadata;
-    }
-
-    public T GetMetadata<T>()
-    {
-        return (T) _metadata;
     }
 }
