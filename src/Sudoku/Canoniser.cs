bcb92f7710809383290ff42610faae69c54dc5e7
Stevo John <stevehuwjohn@gmail.com>
2025-11-15 14:18:30 +0000

    WIP

--- src/Sudoku/Canoniser.cs
+++ src/Sudoku/Canoniser.cs
@@ -39,4 +39,41 @@ public static class Canoniser
             }
         }
     }
+
+    private static void PermuteBand(Span<int> puzzle, int band)
+    {
+        for (var i = 0; i < 3; i++)
+        {
+        }
+    }
+
+    private static int Compare(ReadOnlySpan<int> left, ReadOnlySpan<int> right)
+    {
+        for (var i = 0; i < 9; i++)
+        {
+            if (left[i] < right[i])
+            {
+                return -1;
+            }
+
+            if (right[i] < left[i])
+            {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private static void SwapRows(Span<int> puzzle, int firstRow, int secondRow)
+    {
+        firstRow *= 9;
+
+        secondRow *= 9;
+        
+        for (var i = 0; i < 9; i++)
+        {
+            (puzzle[firstRow + i], puzzle[secondRow + i]) = (puzzle[secondRow + i], puzzle[firstRow + i]);
+        }
+    }
 }
